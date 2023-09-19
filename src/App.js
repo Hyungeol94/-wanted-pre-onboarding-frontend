0cccbfbdfc824cceffc40c2869c9aecd5e29595e
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-09-19 12:54:18 +0900

    Fix: fix bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom'; // Note the correct import
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Note the correct import
 import First from './First';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
@@ -9,21 +9,21 @@ import './App.css';
 const App = () => {
   //로컬 스토리지에서 토큰 가져오기
   const token = localStorage.getItem('jwt');
+  // const navigate = useNavigate();
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<First />} />
-        <Route path="/signin">
-          {token ? <Redirect to="/todo" /> : <SignIn />}
-        </Route>      
-        <Route path="/signup">
-          {token ? <Redirect to="/todo" /> : <SignUp />}
-        </Route> 
-        <Route path="/todo">
-          {token ? <Todo /> : <Redirect to="/signin" />}
-        </Route>
-        <Redirect from="/" to="/signin" />
+        <Route path="/signin"
+              element = {token ? <Navigate to='/todo'/>: <SignIn/>}
+        />
+        <Route path="/signup"
+              element = {token ? <Navigate to='/todo'/> : <SignUp/>}
+        />            
+        <Route path="/todo"
+              element = {token ? <Todo /> : <Navigate to='/signin'/>}
+        />    
       </Routes>
     </BrowserRouter>
   );
