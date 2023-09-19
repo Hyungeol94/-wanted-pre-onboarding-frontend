622fbdfd36f3e0323c3d2fed606de31dd16730d1
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-09-19 18:15:04 +0900

    Fix: 로그인 시 API 통신 오류 해결

--- src/Signin.js
+++ src/Signin.js
@@ -42,27 +42,31 @@ const SignIn = () => {
         console.log('changed')
     }
 
-    const handleSignin = async () => {
+    const handleSignIn = async () => {
         try {
           // 로그인 API 호출 및 응답 받기
-          const response = await fetch('로그인 API 엔드포인트 URL', {
+          const response = await fetch('https://www.pre-onboarding-selection-task.shop/auth/signin', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({email, password }),
+            body: JSON.stringify({
+              email: email,
+              password: password }),
           });
     
           if (response.ok) {
             // 로그인이 성공한 경우
             const data = await response.json();
             // JWT를 로컬 스토리지에 저장
-            localStorage.setItem('jwt', data.token);
+            localStorage.setItem('jwt', data.body);
             // 로그인 성공 시
             navigate('/todo');
           } else {
             // 로그인이 실패한 경우
             console.error('로그인 실패');
+            const responseBody = await response.text();
+            console.error('Response Body:', responseBody);
           }
         } catch (error) {
           console.error('오류 발생:', error);
@@ -84,7 +88,7 @@ const SignIn = () => {
                 </div>
                 <button data-testid="signin-button" 
                         disabled ={!(isValidPassword & isValidEmail)}
-                        onClick = {handleSignin}     
+                        onClick = {handleSignIn}     
                         >로그인</button>
             </Paper>        
         </>
