820d39c1dbefc6653eb53da35fcf5e2801a3b11d
Hyungeol Lee <hyungeol@Hyungeolui-MacBookAir.local>
2023-09-20 01:30:48 +0900

    Feat: getToDos

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,15 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 const Todo = () => {
-    const [toDoList, setToDoList] = useState([{
-      'id' : 1,
-      'todo' : "과제하기",
-      'isCompleted' : false,
-      'userId' : 1
-    }])
+    const [toDoList, setToDoList] = useState([])
     const inputRef = useRef('')
     const token = localStorage.getItem('jwt');
-
+    // Fetch and update the to-do list when the component mounts
+    useEffect(() => {
+      if (token) {
+        getToDos();
+      }
+    }, [token]);
   
     const createToDo = async () => {
         const response = await fetch('https://www.pre-onboarding-selection-task.shop/todos',{
@@ -26,46 +26,38 @@ const Todo = () => {
             console.error('목록 추가 실패:', responseBody);
             throw new Error(`실패 상태: ${response.status}`);            
           }
-          
-        
+
           const newToDo = await response.json();
           console.log('newToDo:', newToDo);          
           setToDoList([...toDoList, newToDo])
+    }
+
+    const getToDos = async () => {
+      console.log('getToDos called ')
+      // Make a GET request to the /todos endpoint
+      fetch('https://www.pre-onboarding-selection-task.shop/todos', {
+          method: 'GET',
+          headers:  
+            {'Authorization': `Bearer ${token}`},
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
         }
-    
-    
-
-    // const getToDos = async () => {
-    //     // Make a GET request to the /todos endpoint
-    //     fetch('https://www.pre-onboarding-selection-task.shop/todos', {
-    //         method: 'GET',
-    //         headers: JSON.stringify({Authorization: token}),
-    //     })
-    //     .then((response) => {
-    //       if (!response.ok) {
-    //         throw new Error(`Request failed with status: ${response.status}`);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then((data) => {
-    //       // Handle the response data
-    //         const parsedData = JSON.parse(data);
-    //         const newToDoList = parsedData.body;
-    //       //console.log('to-dos:', data.body);
-        
-    //       toDoList = JSON.parse(data).body
-    //       setToDoList(newToDoList);
-    //     })
-
-        
-    //       // Update the to-do list or perform other actions as needed
-    //       // For example, you can update the to-do list state here:
-    //     .catch((error) => {
-    //       console.error('Error:', error);
-    //     });
-    // }
-
-
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        setToDoList(data)
+      })
+                
+        // Update the to-do list or perform other actions as needed
+        // For example, you can update the to-do list state here:
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+      
     const updatTodo = () => {
 
     }
@@ -74,7 +66,6 @@ const Todo = () => {
         <>
             <input data-testid="new-todo-input" ref = {inputRef}/>
             <button data-testid="new-todo-add-button" onClick = {createToDo}>추가</button>
-            {/* {getToDos} */}
             <ul>
                 {toDoList.map((info) => (
                 <li key={info.id}>
